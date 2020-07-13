74f1edb0dd2dc97491536fee99db615626badcb5
MarkTselikov <mark.tselikov@gmail.com>
2020-07-14 00:25:33 +0200

    Main code commit

---
+++ Assignment/Model/APIClient.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Model
+{
+    class APIClient
+    {
+        RestClient client;
+
+        public APIClient()
+        {
+            client = new RestClient("https://my.api.mockaroo.com/");
+        }
+
+
+        /*
+         *  This method returns a list of users as list of objects that are parsed from a JSON file received from the API. 
+         *  Both RestSharp and Json.Net libraries are used in this method.
+         */
+        public List<User> GetUsers()
+        {
+            var request = new RestRequest("users.json?key=c69dde60", DataFormat.Json);
+            var response = client.Get<User>(request);
+
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(response.Content);
+            return users;
+        }
+
+
+        /*
+         * This method gets a user by their specified ID.
+         * Please note that this method uses API to get a specific user, but doesn't get all the users and filter them locally. 
+         */
+        public User GetUserByID(int id)
+        {
+            var request = new RestRequest("users/{id}.json?key=c69dde60", DataFormat.Json);
+            request.AddParameter("id", id, ParameterType.UrlSegment);
+            var response = client.Get<User>(request);
+
+            User user = JsonConvert.DeserializeObject<User>(response.Content);
+            return user;
+        }
+
+
+        /*
+         * This method gets users by sending a request to the API and then filters them by the IP locally.
+         * 
+         * Note: this case is probably not working since API generates new IP addresses every time a request is made.
+         * I've made this piece of code to show how I would filter the users locally in case API wasn't desinged to do so.
+         */
+        public List<User> GetUserByIP(String ip)
+        {
+            List<User> users = GetUsers();
+            users = users.Where(u => u.IPAddress.Equals(ip)).ToList();
+            return users;
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,97 @@
+﻿using Assignment.Model;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Assignment
+{
+    class Program
+    {
+        static APIClient client;
+
+        static void Main(string[] args)
+        {
+
+            client = new APIClient();
+            ShowMenu();
+        }
+
+
+        /*
+         * This is a simple method that displays a menu and loops until exit is selected. 
+         * It uses console for inputs and switch-case to navigate the options.
+         */
+        public static void ShowMenu()
+        {
+            var choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("Select an option from the menu below:");
+                Console.WriteLine("\t1. Get all the users.");
+                Console.WriteLine("\t2. Get a user by ID.");
+                Console.WriteLine("\t3. Get a user by IP address.");
+                Console.WriteLine("\t0. Exit.");
+
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        List<User> users = client.GetUsers();
+                        foreach (var u in users)
+                        {
+                            Console.WriteLine(u);
+                        }
+                        break;
+
+                    case 2:
+                        int id;
+                        Console.WriteLine("Please enter the ID:");
+                        id = int.Parse(Console.ReadLine());
+
+                        User user = client.GetUserByID(id);
+                        if(user != null)
+                            Console.WriteLine(user);
+                        else
+                            Console.WriteLine("No user with ID = {0} exists.", id);
+                        break;
+
+                    
+                    case 3:
+                        String ip;
+                        Console.WriteLine("Please enter the IP address:");
+                        ip = Console.ReadLine();
+
+                        List<User> userList = client.GetUserByIP(ip);
+                        if(userList.Count > 0)
+                        {
+                            Console.WriteLine(userList.Count);
+                            foreach (var item in userList)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            
+                        }
+                        else
+                            Console.WriteLine("No users with IP address {0} exist.", ip);
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
